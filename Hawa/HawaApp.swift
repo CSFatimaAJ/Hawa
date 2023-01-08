8ae58f52c67166aca1fa5f5ac5386b0f184c9ed8
CSFatimaAJ <csfatimaaljaber@icloud.com>
2023-01-08 10:24:25 +0300

    Initial Commit

---
+++ Hawa/HawaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawaApp.swift
+//  Hawa
+//
+//  Created by Fatima Aljaber on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HawaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
